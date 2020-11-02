00c30cb7ee64dd94f8ddbc470e668b791f119723
Behzod Khudoyberdiev <khudoyberdievgates1998@gmail.com>
2020-11-02 12:50:27 +0500

    modal

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(".header-wrapper__btn").on('click',function(){
+    $("#ex1").modal({
+        escapeClose: false,
+        clickClose: false,
+        showClose: false,
+      });
+      console.log(1)
+})
